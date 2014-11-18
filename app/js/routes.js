fc46e867471b506781ad5c220fb13ae6a808e536
Rico Sta. Cruz <hi@ricostacruz.com>
2014-11-18 15:03:37 +0800

    Update

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,8 +3,13 @@ page('/article/:id', function (ctx) {
   $(".content-area").show();
 
   // update content
-  var title = $("#link-"+ctx.params.id).text();
+  var $link = $("#link-"+ctx.params.id);
+  var title = $link.text();
   $("h1[data-bind='article-title']").html(title);
+
+  // highlight
+  $("a.selected").removeClass('selected');
+  $link.addClass('selected');
 
   $(".comment-item").hide();
   NProgress.start();
